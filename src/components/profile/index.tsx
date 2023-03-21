59a65caf1846dd13ee573077784386f120e7cce9
giovani <giovaniocan@gmail.com>
2023-03-21 14:18:42 -0300

    css from profile is finished

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,9 +1,50 @@
-import { ProfileContainer } from './styles'
-
+import {
+  ProfileContainer,
+  ContenteProfile,
+  ContactInfo,
+  Eachinfo,
+  HeaderProfile,
+} from './styles'
+import {
+  faArrowUpRightFromSquare,
+  faBuilding,
+  faUserGroup,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 export function Profile() {
   return (
     <ProfileContainer>
-      <h1>profile</h1>
+      <img src="https://github.com/giovaniocan.png" alt="" />
+      <ContenteProfile>
+        <HeaderProfile>
+          <h2>Giovani Apolinario </h2>
+          <div>
+            <h3>GITHUB</h3>
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </div>
+        </HeaderProfile>
+
+        <span>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </span>
+        <ContactInfo>
+          <Eachinfo>
+            <FontAwesomeIcon icon={faGithub} />
+            <span>giovaniocan</span>
+          </Eachinfo>
+          <Eachinfo>
+            <FontAwesomeIcon icon={faBuilding} />
+            <span>rocketseat</span>
+          </Eachinfo>
+          <Eachinfo>
+            <FontAwesomeIcon icon={faUserGroup} />
+            <span>0 seguidores</span>
+          </Eachinfo>
+        </ContactInfo>
+      </ContenteProfile>
     </ProfileContainer>
   )
 }
--- src/components/profile/styles.ts
+++ src/components/profile/styles.ts
@@ -7,4 +7,57 @@ export const ProfileContainer = styled.div`
   background: ${(props) => props.theme['base-profile']};
   border-radius: 10px;
   box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
+
+  padding: 2rem 2.5rem;
+  display: flex;
+  justify-content: center;
+
+  gap: 2rem;
+  img {
+    width: 9.25rem;
+    height: 9.25rem;
+    border-radius: 8px;
+  }
+`
+
+export const ContenteProfile = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  h2 {
+    color: ${(props) => props.theme['base-title']};
+    font-size: 1.5rem;
+    font-weight: bold;
+    line-height: 130%;
+  }
+
+  span {
+    line-height: 160%;
+  }
+`
+export const HeaderProfile = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    color: ${(props) => props.theme.blue};
+    font-size: 0.75rem;
+  }
+`
+
+export const ContactInfo = styled.div`
+  display: flex;
+  margin-top: 1rem;
+  gap: 1.5rem;
+`
+
+export const Eachinfo = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  justify-content: center;
+  align-items: center;
 `
